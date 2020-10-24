2deebf83f959f61f7bea8608d9b75c0c53f4866e
Tommy Thongsythavong <tommythongsythavong@gmail.com>
2020-10-24 11:49:06 -0400

    This is a new test file

--- StockMarket.py
+++ StockMarket.py
@@ -1,1 +1,34 @@
-print("Hello World")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+print(pd.__version__)
+print(yf.__version__)
+print(np.__version__)
+
+msft = yf.Ticker("MSFT")
+print(msft)
+"""
+returns
+<yfinance.Ticker object at 0x1a1715e898>
+"""
+
+# get stock info
+msft.info
+
+"""
+returns:
+{
+ 'quoteType': 'EQUITY',
+ 'quoteSourceName': 'Nasdaq Real Time Price',
+ 'currency': 'USD',
+ 'shortName': 'Microsoft Corporation',
+ 'exchangeTimezoneName': 'America/New_York',
+  ...
+ 'symbol': 'MSFT'
+}
+"""
+
+# get historical market data, here max is 5 years.
+print(msft.history(period="max"))
